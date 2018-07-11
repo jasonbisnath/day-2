e100fd87db74849846b4f8a5d90d590d7a5b9556
Jason Bisnath <jasonbisnath@Jasons-MacBook-Pro.local>
2018-07-11 17:08:02 -0400

    adding files

--- exercises/script.js
+++ exercises/script.js
@@ -1,59 +1,81 @@
 document.querySelector('.puzzle-0').addEventListener('click', function() {
-	  window.alert('Hello, World!');
+	  alert("Hello World!");
 	});
 
 	document.querySelector('.puzzle-1').addEventListener('click', function() {
-		
+		alert("Bonjour Monde!");
 	});
 
 	document.querySelector('.puzzle-2').addEventListener('click', function() {
 
+
 	});
 
 	document.querySelector('.puzzle-3').addEventListener('click', function() {
-
+		document.querySelector('img').src = "https://media.giphy.com/media/WXB88TeARFVvi/giphy.gif"
 	});
 
 	document.querySelector('.puzzle-4').addEventListener('click', function() {
+		document.querySelector('img').style.visibility = "hidden";
 
 	});
 
 	document.querySelector('.puzzle-5').addEventListener('click', function() {
+		document.querySelector('img').style.visibility = "visible";
 
 	});
 
 	document.querySelector('.puzzle-6-yellow').addEventListener('click', function() {
-		
+		document.querySelector('html').style.background = "yellow";
+
 	});
 
 	document.querySelector('.puzzle-6-pink').addEventListener('click', function() {
+		document.querySelector('html').style.background = "pink";
 		
 	});
 
 	document.querySelector('.puzzle-6-purple').addEventListener('click', function() {
+		document.querySelector('html').style.background = "purple";
 		
 	});
 
 	document.querySelector('.puzzle-7').addEventListener('click', function() {
+		document.querySelector('body').style.fontFamily = "comic-sans-MS";
+		document.querySelector('body').style.color = "red"
 		
 	});
 
 	document.querySelector('.puzzle-8').addEventListener('click', function() {
+		document.querySelector('a').href = "http://youtube.com";
 
 	});
 
 	// Challenge puzzles
 
-	document.querySelector('.puzzle-9')
+	document.querySelector('.puzzle-9').addEventListener('click', function() {
+		document.querySelector('body').style.fontFamily = "Monaco";
 
-	document.querySelector('.puzzle-10')
+	});
 
-	document.querySelector('.puzzle-11')
+	document.querySelector('.puzzle-10').addEventListener('click', function() {
+		document.querySelector('audio').play();
 
-	document.querySelector('.puzzle-12')
+	});
+
+	document.querySelector('.puzzle-11').addEventListener('click', function() {
+		document.querySelector('.whatever-i-want').innerHTML <img src="https://pbs.twimg.com/profile_images/959193913264353281/tpvQ-KR3_400x400.jpg>'";
+	}); 
+
+	document.querySelector('.puzzle-12').addEventListener('click', function() {
+		document.querySelector('body').style.color = "blue"
+
+	});
 
 	document.querySelector('.puzzle-13')
 
-	document.querySelector('.puzzle-14')
+	document.querySelector('.puzzle-14').addEventListener('click', function() {
+ 	// document.querySelector('.reee')
+	});
 
 	document.querySelector('.puzzle-15')
